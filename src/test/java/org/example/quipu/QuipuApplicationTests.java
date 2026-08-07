263fa316390efee5c676481308f829fc190ed141
César Urías <cesarestudios14@hotmail.com>
2026-08-06 18:03:41 -0700

    feat(quipu): initial commit from Spring Boot 4.1 scaffold with actuator health check

---
+++ src/test/java/org/example/quipu/QuipuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.quipu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuipuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
